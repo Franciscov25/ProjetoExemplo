601d154c3e683b214174ce1e8122fc2ca9b01424
Francisco <franciscovargasmarcal@gmail.com>
2024-09-19 11:07:51 -0300

    Declarando os tipos de dados var, let, const

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("Olá mundo-externo")
+// alert("Olá mundo-externo")
+
+// DECLARANDO AS VARIÁVEIS
+
+var nome = "fiap";
+console.log(nome)
+
+let nome1 = "cidade"
+console.log(nome1)
+
+const nome2 = "javascript";
+console.log(nome2)
