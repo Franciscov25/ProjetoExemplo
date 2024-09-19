1b4ff03fb3ac91143fddffed2114fbd99b6f2331
Francisco <franciscovargasmarcal@gmail.com>
2024-09-19 11:23:14 -0300

    Criando tipos de variáveis

--- app.js
+++ app.js
@@ -20,3 +20,21 @@ console.log(nome3)
 // declaração vazia
 let disciplina = null;
 console.log(disciplina)
+
+// TIPOS DE VARIÁVEIS
+
+// Inteiro - int
+let idade = 18;
+console.log(idade);
+// Flutuantes ou com casas decimais
+let valor = 6.40;
+console.log(valor);
+// Strings (Textos), com as aspas duplas
+let texto = "fiap";
+console.log(texto);
+// Booleanos - Verdadeiro ou Falso
+let aluno = true;
+console.log(aluno);
+// Array - conjunto de informações -> []
+let tecnologias = ["Java", "C#", "HTML","CSS"];
+console.log(tecnologias);
